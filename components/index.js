55624376b28dc4b217b2bf3b3cf1eb05878d7aef
gabrieledarrigo <darrigo.g@gmail.com>
2019-02-22 13:25:52 +0100

    Re enable testing

---
+++ components/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import dom from 'react-dom';
+import Feed from './photos/Feed';
+import Imaginery from './imaginery/Imaginery';
+
+function renderFeed() {
+    const container = document.querySelector('.photos__content');
+
+    if (container) {
+        dom.render(<Feed />, container);
+    }
+}
+
+function renderImaginery() {
+    function getRatio() {
+        const isMobile = window.matchMedia('(max-width: 768px)').matches;
+        const width = window.document.body.clientWidth;
+        const height = (width * 9) / 21;
+
+        return {
+            width: width,
+            height: height,
+            ratio: width / height,
+            isMobile: isMobile
+        };
+    }
+
+    dom.render(<Imaginery getRatio={ getRatio }/>, document.querySelector('.imaginery__placeholder'));
+}
+
+renderFeed();
+//renderImaginery();
+
+
