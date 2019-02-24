851055c71c2fa597f30f31df78567eb0de6e7880
Gabriele D'Arrigowq <darrigo.g@gmail.com>
2019-02-24 21:25:16 +0100

    Configure webpack aliases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const sass = require('@zeit/next-sass');
 
 module.exports = withPlugins([
   [sass],
-]);
+], {
+  webpack(config) {
+    /* eslint-disable-next-line */
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~static': path.join(__dirname, 'static'),
+      '~components': path.join(__dirname, 'components'),
+      '~templates': path.join(__dirname, 'template'),
+    };
+
+    return config;
+  },
+});
