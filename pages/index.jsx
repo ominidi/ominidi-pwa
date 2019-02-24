d3a78742f43f0789f8645e5da5bf7d8f99cc58a5
Gabriele D'Arrigowq <darrigo.g@gmail.com>
2019-02-24 21:38:51 +0100

    Adjust linting rules

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,14 +12,18 @@ const Index = () => (
     </header>
 
     <article>
-      <h3 className="color-white">Premessa</h3>
+      <h3 className="color-white">
+        Premessa
+      </h3>
 
-      <h4 className="color-white">1.1 Definizione</h4>
+      <h4 className="color-white">
+        1.1 Definizione
+      </h4>
 
       <p>
         Gli ominidi (<em><a href="https://it.wikipedia.org/wiki/Hominidae">Hominidae Gray</a></em>, 1825), noti
           anche come grandi scimmie, sono una famiglia di primati risalente al Miocene inferiore, alla quale
-          appartiene l'uomo.
+          appartiene l&apos;uomo.
       </p>
 
       <p>
@@ -31,7 +35,7 @@ const Index = () => (
       <p>
         I primi esemplari di <em><a href="https://it.wikipedia.org/wiki/Australopithecus">Australopithecus</a></em> si evolsero in
         Africa circa 4 milioni di anni fa.
-        Esseri dai numerosi tratti comuni sia alle scimmie antropomorfe che all'uomo, avevano un'andatura tipicamente bipede ma erano ancora familiari all'arrampicarsi sui radi alberi [...][^2]
+        Esseri dai numerosi tratti comuni sia alle scimmie antropomorfe che all&apos;uomo, avevano un&apos;andatura tipicamente bipede ma erano ancora familiari all&apos;arrampicarsi sui radi alberi [...][^2]
       </p>
 
       <p>
@@ -48,8 +52,7 @@ const Index = () => (
 
       <p>
         Un dimorfismo sessuale molto accentuato lascia presupporre che gli <em>Australopithecus</em> vivessero in branco, guidati da un maschio alpha dominante. <br />
-        E' probabile che gli austrolopitechi non usassero utensili (ad eccezione dello <em>
-          <a href="https://it.wikipedia.org/wiki/Australopithecus_garhi">Australopithecus garhi</a>)</em> e che non avessero sviluppato alcuna forma di linguaggio. <br />
+        E&apos; probabile che gli austrolopitechi non usassero utensili (ad eccezione dello <em><a href="https://it.wikipedia.org/wiki/Australopithecus_garhi">Australopithecus garhi</a></em>) e che non avessero sviluppato alcuna forma di linguaggio. <br />
         La comunicazione avveniva unicamente con <strong>versi e gesti</strong>.
       </p>
 
@@ -57,66 +60,72 @@ const Index = () => (
         Di alimentazione principalmente vegetariana, la dieta era composta da bacche, radici e altri cibi di
           natura vegetale; quando costretto si nutriva anche di piccoli insetti e mammiferi[^3].
         <br />
-        L'<em>Australopithecus</em> era principalmente <strong>nomade</strong>.
+        L&apos;<em>Australopithecus</em> era principalmente <strong>nomade</strong>.
       </p>
 
       <h3 className="color-white">[Ir]Razionale</h3>
 
-      <h4 className="color-white">2.1 Causa</h4>
+      <h4 className="color-white">
+        2.1 Causa
+      </h4>
 
       <p>
-        L'<em>Homo sapiens</em>, specie del genere <em>Homo</em> è un <strong>parassita</strong>. <br />
-        Lo studio della storia e dell'evoluzione umana, sin dagli albori delle prime civiltà, dev'essere
+        L&apos;<em>Homo sapiens</em>, specie del genere <em>Homo</em> è un <strong>parassita</strong>. <br />
+        Lo studio della storia e dell&apos;evoluzione umana, sin dagli albori delle prime civiltà, dev&apos;essere
           affrontato in chiave materialistica.
       </p>
 
       <p>
-        Tutti gli sviluppi in ambito sociale, le convenzioni, le religioni, la scienza, l'economia, sono diretta
-          conseguenza della capacità dell'<em>Homo</em> di piegare a suo vantaggio l'ambiente in cui vive e si
+        Tutti gli sviluppi in ambito sociale, le convenzioni, le religioni, la scienza, l&apos;economia, sono diretta
+          conseguenza della capacità dell&apos;<em>Homo</em> di piegare a suo vantaggio l&apos;ambiente in cui vive e si
           riproduce, sfruttando le risorse naturali con <em>rapporti di produzione</em>[^4] che, oltre a
-        caratterizzare l'epoca storica a cui sono applicati (schiavismo, feudalismo e latifondo, capitalismo e
-          globalizzazione) distruggono e rimodellano artificialmente l'ecosistema terrestre.
+        caratterizzare l&apos;epoca storica a cui sono applicati (schiavismo, feudalismo e latifondo, capitalismo e
+          globalizzazione) distruggono e rimodellano artificialmente l&apos;ecosistema terrestre.
       </p>
 
       <p>
-        Nessuna forma di società con cui si è [auto]organizzato l'essere umano è riuscita ad armonizzarsi in modo
+        Nessuna forma di società con cui si è [auto]organizzato l&apos;essere umano è riuscita ad armonizzarsi in modo
           assoluto col pianeta Terra, neanche le più primigenie: <br />
         nè le prime popolazioni nomadi di cacciatori e raccoglitori, da cui sono nate strutture gerarchice
         patriarcali, autoritarie e violente[^5][^6], nè le comunità stanziali di tipo agricolo, che per prime
-          hanno iniziato a modificare l'habitat da cui trarre sostentamento[^7].
+          hanno iniziato a modificare l&apos;habitat da cui trarre sostentamento[^7].
       </p>
 
       <p>
         Le successive strutture sociali evolutesi nel corso del tempo vanno interpretate come continue e mutevoli
           iterazioni sulle basi delle precedenti; un divenire di stravolgimenti economici e politici atti a
-          nutrire l'unica necessità intima e recondita che accomuna il genere umano: <strong>produrre</strong> e <strong>consumare</strong>. <br />
-        Una caratteristica innata dell'<em>Homo</em>, alienante, dannosa, distruttiva, non solo per la natura che lo circonda ma anche per egli stesso.
+          nutrire l&apos;unica necessità intima e recondita che accomuna il genere umano: <strong>produrre</strong> e <strong>consumare</strong>. <br />
+        Una caratteristica innata dell&apos;<em>Homo</em>, alienante, dannosa, distruttiva, non solo per la natura che lo circonda ma anche per egli stesso.
       </p>
 
       <p>
         Le moderne dottrine economico/sociali (ie: capitalismo, socialismo, globalizzazione) hanno fallito nel
           trovare una soluzione sostenibile ed equa, capace di diminuire il divario fra primi ed ultimi; <br />
         viceversa i vari esperimenti utopici (dalle teorizzazioni di Owen[^8] agli ecovillaggi sostenibili,
-        dalle comunità indios brasiliane[^9] fino all'Uruguay di Pepe Mujica) sono difficilmente attuabili su larga scala.
+        dalle comunità indios brasiliane[^9] fino all&apos;Uruguay di Pepe Mujica) sono difficilmente attuabili su larga scala.
       </p>
 
       <p>
         Le probabilità che il futuro porti in grembo una valida alternativa sono esili. <br />
-        Studi su un futura società [dis][u]topica ove la forza produttiva è completamente basata sull'impiego di
-        manodopera robotica illustrano i rischi derivanti dalla convivenza uomo macchina, mentre la creazione di un <em>ASI</em>, un dio alieno e onniscente, simulacro dell'Uno rappresenta la vana speranza di
-        <em>de</em> responsabilizzare l'uomo dalla morte e distruzione procurate al pianeta, affidandosi alla suprema intelligenza di un essere artificiale e perfetto[^10].
+        Studi su un futura società [dis][u]topica ove la forza produttiva è completamente basata sull&apos;impiego di
+        manodopera robotica illustrano i rischi derivanti dalla convivenza uomo macchina, mentre la creazione di un <em>ASI</em>, un dio alieno e onniscente, simulacro dell&apos;Uno rappresenta la vana speranza di
+        <em>de</em> responsabilizzare l&apos;uomo dalla morte e distruzione procurate al pianeta, affidandosi alla suprema intelligenza di un essere artificiale e perfetto[^10].
       </p>
 
-      <h4 className="color-white">2.2 Effetto</h4>
+      <h4 className="color-white">
+        2.2 Effetto
+      </h4>
 
       <p>
-        L'<strong>estinzione</strong> dell'uomo. <br />
+        L&apos;<strong>estinzione</strong> dell&apos;uomo. <br />
         La scomparsa del <em>Sapiens</em> con la diffusione volontaria di un <em>meta</em> virus: gli <strong>ominidi</strong>.
         <br />
         Il ritorno delle scimmie antropomorfe come unica forma di vita umanoide <em>involuta</em> libera di calcare il suolo del pianeta terra.
       </p>
 
-      <h3 className="color-white">Ominidi</h3>
+      <h3 className="color-white">
+        Ominidi
+      </h3>
 
       <p>
         Ominidi è il punto di arrivo di una serie di riflessioni personali in campo filosofico, politico e
@@ -125,41 +134,43 @@ const Index = () => (
       </p>
 
       <p>
-        L'atto visceralmente anarchico di attaccare adesivi è un elemento di disturbo antagonista alla struttura
+        L&apos;atto visceralmente anarchico di attaccare adesivi è un elemento di disturbo antagonista alla struttura
           sociale moderna; <br />
         ogni adesivo, o tag, o manifesto è un ominide, un <em>virus</em> primitivo che proliferando mina le basi
-        dell'arrogante dominio umano sull'ecosistema terrestre, un gutturale urlo di protesta contro la smisurata <em>hybris</em> del <em>Sapiens</em>.
+        dell&apos;arrogante dominio umano sull&apos;ecosistema terrestre, un gutturale urlo di protesta contro la smisurata <em>hybris</em> del <em>Sapiens</em>.
       </p>
 
       <p>
         Ominidi è la somma di diverse esperienze multidisciplinari: street art, design e scrittura per dare una
-          forma concreta a ciò che era solo un'idea e l'informatica come <em>medium</em> per veicolare in rete il
+          forma concreta a ciò che era solo un&apos;idea e l&apos;informatica come <em>medium</em> per veicolare in rete il
           messaggio in una forma libera e senza vincoli.
 
         <br />
 
-        Tutto il materiale di Ominidi, dal codice sorgente dell'applicativo web, al materiale grafico con cui
+        Tutto il materiale di Ominidi, dal codice sorgente dell&apos;applicativo web, al materiale grafico con cui
           sono stati creati i primi adesivi, sino al Manifesto stesso è <em>free</em> ed <em>open source</em>,
           rilasciato con licenze MIT o Creative Commons.
       </p>
 
       <p>
-        Infine Omindi è la valvola di sfogo di una rabbia [com][re]pressa attraverso l'autocontrollo, un viatico
-          per l'impotenza intellettuale e fisica di poter cambiare la realtà che ci circonda, la narrazione di
+        Infine Omindi è la valvola di sfogo di una rabbia [com][re]pressa attraverso l&apos;autocontrollo, un viatico
+          per l&apos;impotenza intellettuale e fisica di poter cambiare la realtà che ci circonda, la narrazione di
           temi affascinanti (anarchia, robot ed AI, graffiti) attraverso suggestioni distopiche ed apocalittiche.
           Ominidi non è nè uomo nè animale.
       </p>
 
-      <h5 className="color-white">Note</h5>
+      <h5 className="color-white">
+        Note
+      </h5>
 
       <p>
-        [^1] Autori vari, <a href="https://it.wikipedia.org/wiki/Australopithecus">Wikipedia</a></p>
+        [^1] Autori vari, <a href="https://it.wikipedia.org/wiki/Australopithecus">Wikipedia</a>
+      </p>
 
       <p>
         [^2] David A. Raichlen, Adam D. Gordon, William E. H. Harcourt-Smith, Adam D. Foster, Wm. Randall Haas,
           Jr, <br />
-        <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2842428/">Laetoli Footprints Preserve Earliest
-              Direct Evidence of Human-Like Bipedal Biomechanics</a>, in PLoS ONE, vol. 5, nº 3, 2010, p. e9769,
+        <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2842428/">Laetoli Footprints Preserve Earliest Direct Evidence of Human-Like Bipedal Biomechanics</a>, in PLoS ONE, vol. 5, nº 3, 2010, p. e9769,
           DOI:10.1371/journal.pone.0009769, PMC 2842428, PMID 20339543.
       </p>
 
@@ -173,7 +184,7 @@ const Index = () => (
       <p>
         [^4] Karl Marx,
         <a href="https://www.marxists.org/italiano/marx-engels/1859/criticaep/">
-        Per la Critica dell'Economia Politica
+        Per la Critica dell&apos;Economia Politica
         </a>, 1859
       </p>
 
@@ -190,14 +201,12 @@ const Index = () => (
       </p>
 
       <p>
-        [^8] <a href="">Per una concezione della società e altri scritti</a>, Bari, Laterza, 1971, pp. 35-47; <a
-        href="http://la.utexas.edu/users/hcleaver/368/368OwenNewViewtable.pdf">A New View of Society</a>,
+        [^8] Per una concezione della società e altri scritti, Bari, Laterza, 1971, pp. 35-47; <a href="http://la.utexas.edu/users/hcleaver/368/368OwenNewViewtable.pdf">A New View of Society</a>,
           New York, MacMillan, 1972, pp. 41-60
       </p>
 
       <p>
-        [^9] Vera Lúcia de Oliveira, <a href="http://www.gaffi.it/document/upload/ingegni/UtopiaSelvaggia.pdf">Utopia
-          Selvaggia</a>,Roma, 2006
+        [^9] Vera Lúcia de Oliveira, <a href="http://www.gaffi.it/document/upload/ingegni/UtopiaSelvaggia.pdf">Utopia Selvaggia</a>, Roma, 2006
       </p>
 
       <p>
