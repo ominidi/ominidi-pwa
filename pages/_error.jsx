6f81e3db83ca6d84e690eb49b3fcf43d9ed6f9df
gabrieledarrigo <darrigo.g@gmail.com>
2019-03-06 23:21:49 +0100

    Use page layout in pages

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import '~static/scss/style.scss';
+import Page from '../layouts/page/Page';
 
 const Error = () => (
-  <section className="section-template error">
-    <header className="section__header">
-      <h2 className="section__title title-2">
-        Errore
-      </h2>
+  <Page title="Errore">
+    <figure className="error__figure">
+      <img src="https://www.researchgate.net/profile/Lars_Taxen/publication/299489921/figure/fig6/AS:652952930906121@1532687518665/Illustration-of-a-mammoth-hunt-Source-Original-wood-engraving-by-E-Bayard-in-Bryant.png" title="Errore" alt="Error" />
+    </figure>
 
-      <figure className="error__figure">
-        <img src="https://www.researchgate.net/profile/Lars_Taxen/publication/299489921/figure/fig6/AS:652952930906121@1532687518665/Illustration-of-a-mammoth-hunt-Source-Original-wood-engraving-by-E-Bayard-in-Bryant.png" title="Errore" alt="Error" />
-      </figure>
-
-      <h3 className="title-4 text-center">
-        Niente da vedere qui, torna all&apos;
-        <a href="/" title="homepage">
-          homepage
-        </a>
-      </h3>
-    </header>
-  </section>
+    <h3 className="title-4 text-center">
+      Niente da vedere qui, torna all&apos;
+      <a href="/" title="homepage">
+        homepage
+      </a>
+    </h3>
+  </Page>
 );
 
 export default Error;
