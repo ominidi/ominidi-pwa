10dc12d02c22002bbc41d8946946a9dfc95bc38c
gabrieledarrigo <darrigo.g@gmail.com>
2019-03-19 21:21:15 +0100

    Fix tests

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -2,3 +2,18 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('../data/common.yml', () => ({
+  navItems: [{
+    href: '/',
+    title: 'Manifest',
+    text: 'Manifest',
+    icon: 'to-manifest',
+  },
+  {
+    href: '/photos',
+    title: 'Photos',
+    text: 'Photos',
+    icon: 'to-photos',
+  }],
+}));
